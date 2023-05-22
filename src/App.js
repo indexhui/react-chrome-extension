06e56017b5b9eb5013db61a7f3614f8ba4a2d1e3
hugh <indexhui@gmail.com>
2023-05-22 08:50:07 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -8,21 +8,46 @@ import {
   Code,
   Grid,
   theme,
+  Button,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 
 function App() {
+  // eslint-disable-next-line no-undef
+  chrome.webRequest?.onBeforeSendHeaders?.addListener(
+    function (details) {
+      var headers = details.requestHeaders;
+      for (var i = 0; i < headers.length; i++) {
+        console.log(headers[i].name);
+        if (headers[i].name === 'X-Forwarded-For') {
+          headers[i].value = '45.76.208.195'; // 修改請求標頭
+          break;
+        }
+      }
+      return { requestHeaders: headers };
+    },
+    { urls: ['<all_urls>'] },
+    ['blocking', 'requestHeaders']
+  );
+  console.log('1');
+  const handleClick = () => {
+    console.log('2');
+    // eslint-disable-next-line no-undef
+    chrome.tabs.create({ url: 'https://www.google.com' });
+  };
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
+      <Box textAlign="center" fontSize="xl" w="320px">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
             <Logo h="40vmin" pointerEvents="none" />
             <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+              Edit <Code fontSize="xl">src/App.js</Code>
             </Text>
+            <Button onClick={handleClick}>test</Button>
             <Link
               color="teal.500"
               href="https://chakra-ui.com"
